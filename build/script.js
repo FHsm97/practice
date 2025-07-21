52c4f3baa1e19744025471449bf57c7f38d8f9c5
FHsm97 <hashemi.fatemeh1379@gmail.com>
2025-07-21 11:49:13 +0330

    4-number,string,boolean types

--- build/script.js
+++ build/script.js
@@ -6,3 +6,9 @@ lastname = 'hashemi';
 console.log(lastname);
 lastname = 55;
 console.log(lastname);
+let age = 25;
+let student = false;
+// let height:string=162; error
+let list = [1, 2, 3];
+// list=['1','2','3'] error
+console.log(list);
--- src/script.ts
+++ src/script.ts
@@ -9,4 +9,16 @@ console.log(lastname);
 lastname=55;
 console.log(lastname);
 
+let age:number=25;
+
+let student:boolean=false;
+
+// let height:string=162; error
+
+
+let list=[1,2,3];
+
+// list=['1','2','3'] error
+
+console.log(list);
 
