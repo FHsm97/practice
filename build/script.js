95f9906df04d099dcfd35f383b2bc3580b67b5c7
FHsm97 <hashemi.fatemeh1379@gmail.com>
2025-07-21 11:39:26 +0330

    3-types basic

--- build/script.js
+++ build/script.js
@@ -1,1 +1,8 @@
 "use strict";
+let firstname = 'fatemeh';
+// firstname=12; error
+let lastname;
+lastname = 'hashemi';
+console.log(lastname);
+lastname = 55;
+console.log(lastname);
---
+++ src/script.ts
@@ -0,0 +1,12 @@
+let firstname='fatemeh';
+// firstname=12; error
+
+let lastname;
+
+lastname='hashemi';
+console.log(lastname);
+
+lastname=55;
+console.log(lastname);
+
+
